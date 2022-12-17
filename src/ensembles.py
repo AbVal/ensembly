3ed23a45acd57ae915154c2c52e21fc6a956f48f
Valentine Abramov <ab.val@mail.ru>
2022-12-17 14:43:39 +0300

    Implemented GradientBoostingMSE, minor fixes in RandomForestMSE

--- src/ensembles.py
+++ src/ensembles.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.optimize import minimize_scalar
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_squared_error
 
 
 class RandomForestMSE:
@@ -21,13 +22,13 @@ class RandomForestMSE:
         self.n_estimators = n_estimators
         self.max_depth = max_depth
         self.feature_subsample_size = feature_subsample_size
+        self.random_state = None
 
         self.tree_list = []
         self.feature_subset_list = []
 
         for param, value in trees_parameters.items():
-            if param == 'random_state':
-                self.random_state = value
+            self.__setattr__(param, value)
 
     def fit(self, X, y, X_val=None, y_val=None):
         """
@@ -100,6 +101,18 @@ class GradientBoostingMSE:
         feature_subsample_size : float
             The size of feature set for each tree. If None then use one-third of all features.
         """
+        self.n_estimators = n_estimators
+        self.learning_rate = learning_rate
+        self.max_depth = max_depth
+        self.feature_subsample_size = feature_subsample_size
+        self.random_state = None
+
+        self.tree_list = []
+        self.alpha_list = []
+        self.feature_subset_list = []
+
+        for param, value in trees_parameters.items():
+            self.__setattr__(param, value)
 
     def fit(self, X, y, X_val=None, y_val=None):
         """
@@ -109,6 +122,30 @@ class GradientBoostingMSE:
         y : numpy ndarray
             Array of size n_objects
         """
+        np.random.seed(self.random_state)
+
+        if self.feature_subsample_size is None:
+            self.feature_subsample_size = X.shape[1] // 3
+        elif isinstance(self.feature_subsample_size, float):
+            self.feature_subsample_size = int(X.shape[1] * self.feature_subsample_size)
+
+        y_pred = np.zeros_like(y)
+
+        for _ in range(self.n_estimators):
+            antigrad = 2 * (y - y_pred)
+            feature = np.random.choice(X.shape[1], self.feature_subsample_size, replace=False)
+
+            tree = DecisionTreeRegressor(max_depth=self.max_depth, random_state=self.random_state)
+            tree.fit(X[:, feature], antigrad)
+            tree_pred = tree.predict(X)
+
+            alpha = minimize_scalar(lambda x, tp=tree_pred: mean_squared_error(y, y_pred + x * tp)).x
+
+            self.tree_list.append(tree)
+            self.alpha_list.append(alpha)
+            self.feature_subset_list.append(feature)
+
+            y_pred = y_pred + alpha * self.learning_rate * tree_pred
 
     def predict(self, X):
         """
@@ -120,3 +157,9 @@ class GradientBoostingMSE:
         y : numpy ndarray
             Array of size n_objects
         """
+        prediction = np.zeros(X.shape[0])
+
+        for tree, feature_subset, alpha in zip(self.tree_list, self.feature_subset_list, self.alpha_list):
+            prediction += alpha * self.learning_rate * tree.predict(X[:, feature_subset])
+
+        return prediction
