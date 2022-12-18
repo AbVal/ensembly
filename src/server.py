da3c6f14ecd3e2160eaab13cf06d7aa4ce58d198
Valentine Abramov <ab.val@mail.ru>
2022-12-18 18:47:41 +0300

    Added basic web interface

--- src/server.py
+++ src/server.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import numpy as np
+import pandas as pd
+from ensembles import RandomForestMSE, GradientBoostingMSE
+import plotly.express as px
+import plotly.graph_objects as go
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello World!'
+@app.route('/', methods=['GET'])
+def start_page():
+    return render_template('index.html')
 
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+        train = request.files['train']
+        valid = request.files['valid']
+        print(train)
+        print(valid)
+        n_estimators = request.form['n_estimators']
+        feat = request.form['feature_scale']
+        max_depth = request.form['max_depth']
+        learning_rate = request.form['learning_rate']
+
+        print(n_estimators, feat, max_depth, learning_rate)
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
