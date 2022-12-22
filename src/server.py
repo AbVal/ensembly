684dae4da2aa9c89c7c4e16ab2986fdd54b78c44
Valentine Abramov <ab.val@mail.ru>
2022-12-22 21:39:33 +0300

    Added prediction, removed temp file storing, other minor fixes

--- src/server.py
+++ src/server.py
@@ -1,4 +1,4 @@
-import uuid
+import io
 import numpy as np
 import pandas as pd
 import plotly.express as px
@@ -6,11 +6,11 @@ from flask import Flask, render_template, request, redirect, url_for
 from ensembles import RandomForestMSE, GradientBoostingMSE
 
 # TODO:
-# predictions
 # docstrings
 # css
 
 app = Flask(__name__)
+model = None
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -20,12 +20,10 @@ def start_page():
 
 @app.route('/train', methods=['GET', 'POST'])
 def train():
+    global model
     if request.method == 'POST':
         train = request.files['train']
         valid = request.files['valid']
-        showflag = request.form.get('show_graph')
-        if showflag is None:
-            showflag = False
 
         model_name = request.form['model']
 
@@ -57,39 +55,53 @@ def train():
         if max_depth is not None and max_depth <= 0:
             return redirect(url_for('start_page'))
 
-        # df = pd.read_csv(train)
-        # TODO:
-        # dataframe view
-        # return render_template('pd_df.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
-
         X_train = pd.read_csv(train)
         X_valid = pd.read_csv(valid)
 
         model, hist = train_model(model_name, n_estimators, feat, max_depth, learning_rate, X_train, X_valid)
 
-        random_string = uuid.uuid4().hex
-        path = "static/temp/" + random_string + ".svg"
+        buffer = io.StringIO()
+        fig = make_picture(hist)
+        fig.write_html(buffer)
+        graph_html = buffer.getvalue()
 
-        make_picture(hist, path, showflag)
-
-        return render_template('predict.html', href=path,
+        return render_template('predict.html',
                                n_estimators=model.n_estimators,
                                feature_scale=model.feature_subsample_size,
                                max_depth=model.max_depth,
                                learning_rate=learning_rate,
                                model_name=model_name,
                                tables=[X_train.sample(100).to_html(classes='Обучающая выборка')],
-                               titles=X_train.columns.values)
+                               titles=X_train.columns.values,
+                               graph=graph_html)
 
     return redirect(url_for('start_page'))
 
 
-# TODO:
-# send arguments through url
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
+    global model
     if request.method == 'POST':
         test = request.files['test']
+        X_test = pd.read_csv(test)
+        X_test_numpy = X_test.drop(X_test.columns.values[X_test.dtypes == 'object'], axis=1).to_numpy()
+
+        y_pred = model.predict(X_test_numpy)
+
+        display_df = request.form['display_df']
+
+        if display_df == 'yes':
+            X_test['Предсказание'] = y_pred
+
+            return render_template('results.html',
+                                   tables=[X_test.to_html(classes='Загруженная выборка')],
+                                   titles=X_test.columns.values)
+
+        pred_df = pd.DataFrame(y_pred, columns=['prediction'])
+        return render_template('results.html',
+                               tables=[pred_df.to_html(classes='Загруженная выборка')],
+                               titles=pred_df.columns.values)
+
     return redirect(url_for('start_page'))
 
 
@@ -119,7 +131,7 @@ def train_model(model_name, n_estimators, feat, max_depth, learning_rate, X_trai
     return model, hist
 
 
-def make_picture(history, output_file, showflag):
+def make_picture(history):
     n_estimators = np.arange(len(history['rmse_train']))
 
     df_dict = {'Число деревьев': n_estimators,
@@ -131,7 +143,7 @@ def make_picture(history, output_file, showflag):
     fig = px.line(df, x='Число деревьев',
                   y=['Обучающая выборка', 'Валидационная выборка'],
                   title="RMSE от количества деревьев",
-                  labels={'x': 'Число деревьев', 'value': 'RMSE'})
+                  labels={'x': 'Число деревьев', 'value': 'RMSE'}, height=600)
 
     fig.update_layout(legend=dict(
         title='',
@@ -141,10 +153,8 @@ def make_picture(history, output_file, showflag):
         x=1
     ))
 
-    fig.write_image(output_file, width=800, engine='kaleido')
+    return fig
 
-    if showflag:
-        fig.show()
 
 if __name__ == '__main__':
     app.run(debug=True)
